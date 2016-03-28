dacdeafc4ad884bdf7f5c2d337eb4d53fbe11482
Michal Klimuk <michal.klimuk@neo9.pl>
2016-03-28 22:07:34 +0200

    initial config

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' < registry.yml > registry-config.yml
+awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' < compse.yml > docker-compose.yml
