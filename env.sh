65eb9cd42f0c1618d2db650c50ae76fa789827c7
root <root@ip-172-31-22-50.eu-central-1.compute.internal>
2016-03-28 20:38:07 +0000

    fix errors

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' < registry.yml > registry-config.yml
-awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' < compse.yml > docker-compose.yml
+awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' < compose.yml > docker-compose.yml
+
